ae33ee82177ae7fb7e04a502d49ac833bd8d462e
Sh4d0w <266088743+unkn0wn-sh4d0w@users.noreply.github.com>
2026-04-19 18:00:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,23 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');
+const CACHE_VERSION = "3";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_VERSION) return caches.delete(key);
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
